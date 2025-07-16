51e0dce532c34ecd028c3ee597b38be609f73818
Ever C. <everjcruz@hotmail.com>
2025-07-16 17:11:20 -0400

    Add one_wikitable function to fetch one Wikipedia table and create a CSV file

--- wikipedia_table_scraper.py
+++ wikipedia_table_scraper.py
@@ -14,3 +14,23 @@ def get_url_info(url):
     
     soup = BeautifulSoup(response.text, "html.parser")
     return soup
+
+def one_wikitable(soup, title):
+    table = soup.find('table', {'class': 'wikitable'})
+    rows = table.find_all("tr")
+    
+    filename = f"{title}_wikipedia_table.csv"
+    with open(filename, "w", newline='', encoding="uft-8") as file:
+        writer = csv.writer(file)
+        
+        for row in rows:
+            columns = row.find_all(["th", "td"])
+            rows_data = []
+            
+            for column in columns:
+                text = columns.text.strip()
+                rows_data.append(text)
+                
+                if rows_data:
+                    writer.writerow(rows_data)
+    print(f"CSV File for {title} has been created.")
